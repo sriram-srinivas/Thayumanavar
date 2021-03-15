9815d0e9e7dbd4faeb1dbb0acc4d4f7665d62baa
sriram_srinivas <sriram_s@soalrtis.com>
2021-03-15 21:27:25 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter, Route} from 'react-router-dom';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter >
+      <Route render={props =>  
+      <App  {...props}/> } />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/sections/Content.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Content = props =>{
+    return(
+        <div className="Content"> Content</div>
+    )
+}
+
+export default Content
